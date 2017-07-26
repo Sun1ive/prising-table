5f7eb19637ca6c49afca6149055df6f855a8450b
Sergey <sunliveua@gmail.com>
2017-07-26 20:58:23 +0300

    version 1.0.0
    responsive mobile version + some scripts

--- _scripts/common.js
+++ _scripts/common.js
@@ -1,1 +1,17 @@
-//SCRIPTS
+$(function() {
+	$(".lowResMenu").click(function(event) {
+		$(this).children(".subMenu").slideToggle("slow");
+	});
+});
+
+$(window).on("load",function() {
+	let windowHeight = $(document).height();
+	let speedStep = 2;
+	let timeScrolling = windowHeight / speedStep;
+
+	$(".scrollTop").click(function(event) {
+		$("html, body").animate({
+			scrollTop: 0
+		}, timeScrolling);
+	});
+});
