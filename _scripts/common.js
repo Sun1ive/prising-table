241b1c71c8795b90e9c7a65263421209cc4acf52
Sergey <sunliveua@gmail.com>
2017-07-27 12:10:49 +0300

    0.0.5
    fix

--- _scripts/common.js
+++ _scripts/common.js
@@ -6,12 +6,18 @@ $(function() {
 
 $(window).on("load",function() {
 	let windowHeight = $(document).height();
-	let speedStep = 2;
+	let speedStep = 3;
 	let timeScrolling = windowHeight / speedStep;
 
 	$(".scrollTop").click(function(event) {
 		$("html, body").animate({
 			scrollTop: 0
 		}, timeScrolling);
+	});
+
+	$(".main-arrow").click(function(event) {
+		$("html, body").animate({
+			scrollTop: windowHeight
+		}, 2500);
 	});
 });
